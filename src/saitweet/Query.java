284ad47dd828bca436d52eec2682bd42fcafd160
albertusk95 <albertusk15@gmail.com>
2016-08-15 17:41:33 +0700

    added Query

---
+++ src/saitweet/Query.java
@@ -0,0 +1,12 @@
+package saitweet;
+
+public class Query {
+
+	public static String myQuery;
+	public static String[] socmedCheck;
+	
+	public static void setQuery(String myQuery, String[] socmedCheck) {
+		Query.myQuery = myQuery;
+		Query.socmedCheck = socmedCheck;
+	}
+}
