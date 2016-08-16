1a74ec61d1da8e19abddfaf9f1bc4af2e66c2569
albertusk95 <albertusk15@gmail.com>
2016-08-16 19:20:07 +0700

    servlet and custom code

--- src/myservlet/FormService.java
+++ src/myservlet/FormService.java
@@ -1,14 +1,24 @@
 package myservlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import saitweet.Query;
 import saitweet.Tweet;
+import twitter4j.TwitterException;
 
 /**
  * Servlet implementation class FormService
@@ -37,20 +47,44 @@ public class FormService extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		//response.setContentType("text/html");
+        //response.setCharacterEncoding("utf-8");
+	       
 		// query text
-		String query = request.getParameter("query");
+		//String query = request.getParameter("query");
 		
 		// checkbox value
-		String[] socmedCheck = request.getParameterValues("socmed_check");
+		//String[] socmedCheck = request.getParameterValues("socmed_check");
 		
 		// set query and chosen social media as global value
-		Query.setQuery(query, socmedCheck);
+		//Query.setQuery(query, socmedCheck);
 		
-		Tweet.extractTweet(query);
+		// extract Twitter data
+		/*
+		try {
+			Tweet.extractTweet(query);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+		*/
+		
+		// debugging
+		System.out.println("HELLO DOPOST");
 		
 		// redirect to primary.jsp with the query text
-		request.setAttribute("query", query);
-        request.getRequestDispatcher("primary.jsp").forward(request, response);
+		//request.setAttribute("query", query);
+		//request.getRequestDispatcher("#/primary").forward(request, response);
+		//response.sendRedirect("localhost:8080/TwitterServlet/#/primary"); 
+		
+    	String contextPath = request.getContextPath();
+    	System.out.println("context: " + contextPath);
+    	System.out.println("servlet: " + request.getServletContext());
+		
+		//RequestDispatcher rd = request.getRequestDispatcher("/#/primary") ;
+    	//rd.forward(request, response) ;
+    	
+		response.sendRedirect(contextPath + "#/primary"); 
+		
 	}
 
 }
--- src/saitweet/Tweet.java
+++ src/saitweet/Tweet.java
@@ -1,8 +1,54 @@
 package saitweet;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Tweet {
 
-	public static void extractTweet(String tweet) {
+	public static List<Status> qrTweets;
+	
+	/*
+	 * Extract Twitter data
+	 */
+	public static void extractTweet(String tweet) throws TwitterException {
 		
-	}
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		
+		cb.setDebugEnabled(true);
+		cb.setOAuthConsumerKey("74ydo2PwWnePf3X4zBzleNJ2t");
+		cb.setOAuthConsumerSecret("KfIjEiPnosHTf1XZieGWXrldtDQWKFFh1jO63dxfjGqYDp2bLd");
+		cb.setOAuthAccessToken("2729380164-fQvHtakqqd8Uuwi5BFAGb7bjANsV6FZzMDHfkZg");
+		cb.setOAuthAccessTokenSecret("fDBcQDeiaBH1BVUVJ1OduRdsHTs4ozZMScOY8KYqgd0It");
+		
+		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+		
+		//List<String> result = new LinkedList<String>();
+		
+		Query query = new Query(tweet+" +exclude:retweets");
+		query.setCount(3); 	// set tweets per page to 3
+		query.setLang("en");
+		
+		QueryResult qr = twitter.search(query);
+		qrTweets = qr.getTweets();
+		
+		//Preprocessor pp = new Preprocessor();
+ 		
+		System.out.println("Extracted tweets");
+		
+		for(Status t : qrTweets) {
+ 			System.out.println(t.getText());
+ 			//tmp = pp.preprocessDocumentKeepSmiles(t.getText());
+ 			//tmp = tmp.replaceAll("[\n\r]", "");
+ 			//System.out.println("\tPreprocessed tweet: "+tmp);
+ 			//result.add("?,"+Utils.quote(tmp));
+ 		}
+	 }
 }
